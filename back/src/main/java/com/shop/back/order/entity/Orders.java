fbfa40fe9ec87545f21cdaebcdce50f1d616bc03
Sooyeon Noh <tndus1739@gmail.com>
2024-05-03 19:32:41 +0900

    commit

--- back/src/main/java/com/shop/back/order/entity/Orders.java
+++ back/src/main/java/com/shop/back/order/entity/Orders.java
@@ -1,5 +1,8 @@
 package com.shop.back.order.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.shop.back.common.BaseEntity;
 import com.shop.back.item.entity.Item;
 import com.shop.back.member.entity.Member;
@@ -11,24 +14,21 @@ import lombok.Setter;
 @Setter
 @Entity
 public class Orders extends BaseEntity {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	private String code;
-
-	private int cnt;
-
-	private int price;
+	private String status;
 
 	private int del;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "item_id")
-	private Item item;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
-
+	
+	@OneToMany(fetch = FetchType.LAZY , cascade = CascadeType.ALL)
+	@JoinColumn(name = "orders_id")
+	private List<OrderItem> orderItems = new ArrayList<>();
 }
