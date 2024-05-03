ef00f41c5f1c487da68ab50078201db51bcdea64
minhyeok Bang <bmh89895656@gmail.com>
2024-05-03 19:32:49 +0900

    장바구니

--- front/src/views/user/mypage/MyCart.js
+++ front/src/views/user/mypage/MyCart.js
@@ -1,14 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import MyPageTabs from "src/views/user/mypage/MyPageTabs";
-import {CCard, CCardBody, CCol} from "@coreui/react";
+import {CButton, CCard, CCardBody, CCol, CRow} from "@coreui/react";
 import axios from "axios";
+import Cart from "src/views/user/mypage/component/Cart";
 
 const MyCart = () => {
   const [cart, setCart] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const addCommas = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
   const getCart = () => {
     axios.get(`http://localhost:3011/cart/${localStorage.getItem('email')}`).then((res) => {
       console.log(res)
+      setCart(res.data)
+    })
+  }
+
+  const buy = () => {
+
+  }
+
+  const del = (id) => {
+    axios.delete(`http://localhost:3011/cart/cartItem/${id}`).then((res) => {
+      console.log(res)
+      getCart()
     })
   }
 
@@ -17,17 +35,32 @@ const MyCart = () => {
   }, []);
 
   return (
-    <CCol xs={12}>
-      <CCard className="mb-4">
-        <CCardBody>
-          <MyPageTabs/>
-          <CCardBody>
-            {/*내용*/}
-            장바구니
-          </CCardBody>
-        </CCardBody>
-      </CCard>
-    </CCol>
+    <>
+      <CRow>
+        <CCol xs={12}>
+          <CCard className="mb-4">
+            <CCardBody>
+              <MyPageTabs/>
+              <CCardBody>
+                {/*내용*/}
+                장바구니
+                <Cart cart={cart} total={setTotal} setCart={setCart} deleteFnc={del}/>
+                <CRow>
+                  <CCol style={{textAlign: 'center'}}>
+                    <strong style={{fontSize: '23px'}}>전체 금액 : {addCommas(Number(total))}</strong>
+                  </CCol>
+                </CRow>
+              </CCardBody>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+      <CRow>
+        <CCol style={{textAlign: 'center'}}>
+          <CButton color={'dark'} size={'lg'} onClick={buy}>주문하기</CButton>
+        </CCol>
+      </CRow>
+    </>
   );
 };
 
